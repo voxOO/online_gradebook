83bea2858cdd51d0f53103a45c2788ba7f20c9b3
Vojislav <vojislav.stupar@gmail.com>
2019-07-29 18:36:18 +0200

    initial commit

---
+++ src/components/GradebookApp.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+       <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
